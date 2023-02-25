d6b35d81590a56df00b14b51e1767e303f3973f9
Christian Harjuno <harjuno.tian@gmail.com>
2023-02-25 21:53:46 +0900

    fix: slides

---
+++ DSC/slide.js
@@ -0,0 +1,13 @@
+const activeSlide = "IndividualSlide active";
+// Ini cara tolol. Karna buru buru jadi terpaksa pake cara manual. Good luck siapapun yang ngeliat ini di masa depan. <3 kepina
+const prev = () => {
+	const slides = document.querySelectorAll(".IndividualSlide");
+	for (let i = 0; i < 3; i++) {
+		if (slides[i].classList[1] !== "active") continue;
+		slides[i].className = "IndividualSlide";
+		if (i == 2) return (slides[i - 1].className = activeSlide);
+		if (i == 1) return (slides[i - 1].className = activeSlide);
+		if (i == 0) return (slides[2].className = activeSlide);
+	}
+};
+const next = () => {};
